39776f2a9d4d178b1687775d334f2e39e21f89ad
kingcrow15 <61525901+kingcrow15@users.noreply.github.com>
2020-07-19 00:22:06 -0700

    fully fucntional

--- app.py
+++ app.py
@@ -1,22 +1,40 @@
-
-
-from flask import Flask, render_template, redirect
-app = Flask(__name__)
+from flask import Flask 
+from flask import render_template
+from flask import redirect
+from flask import request
 from flask_pymongo import PyMongo
+import scrapeData
+from pymongo import MongoClient
+import pymongo
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'@app.route('/')
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars"
+mongo = PyMongo(app)
 
 @app.route("/")
-@app.route("/home")
-def home():
-    return render_template("index.html")
-    # Find one record of data from the mongo database
-    # mars_info = mongo.db.collection.find_one()
-    # print(mars_info)
-    # Return template and data
-    # , mars_info = mars_info, text = text 
+def index():
+
+    mars = mongo.db.mars.find_one()
+    
+    return render_template("index.html", mars = mars)
+
+ 
+@app.route("/scrape")
+def scrape():
+
+    mars = mongo.db.mars
+    mars_web = scrapeData.scrape_news()
+    mars_web = scrapeData.scrape_Img()
+    mars_web = scrapeData.scrape_marsFact()
+    mars_web = scrapeData.scrape__hemispheres()
+    mars_web = scrapeData.scrape_Weather()
+    
+    
+    mars.update({}, mars_web, upsert=True)
+    
+    return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
